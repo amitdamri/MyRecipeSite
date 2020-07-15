8e93306743297d24a90592729f2cffbc0c572dae
amitdamri <amitdamri@gmail.com>
2020-07-15 15:51:27 +0300

    Create App_20200715154606.vue

---
+++ .history/src/App_20200715154606.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+@import "@/scss/form-style.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-image: url("../resources/dough.jpg");
+  background-repeat: no-repeat;
+  background-size:cover;
+  position: fixed;
+	width: 100%;
+	height: 100px;
+  overflow-y:scroll;
+  overflow-x:hidden;
+
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+</style>
